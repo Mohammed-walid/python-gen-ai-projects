0827e01d072a55386ab9b5a3c8b976efc423da97
Mohamed <mohamedwalid.elkholy@gmail.com>
2026-08-16 09:10:21 +0300

    Implemented the interface.py module

--- Generative-AI-powered-weeting-assistant/interface.py
+++ Generative-AI-powered-weeting-assistant/interface.py
@@ -1,9 +1,21 @@
 """Gradio interface module"""
 import gradio as gr
-
+from speech_to_text import SpeechToText
+from audio_downloader import AudioDownloader
 class Interface:
-    def __init__(self):
-        pass
+    def __init__(self, url):
+        print("Downloading audio")
+        audio = AudioDownloader()
+        audio.download_file(url)
+
+        self.transcriber = SpeechToText()
+        self.audio_input = gr.Audio(sources = "upload", type = "filepath")
+        self.output_text = gr.Textbox()  # Text output
+
+        self.iface = gr.Interface(fn = self.transcriber.transcribe,
+                             inputs = self.audio_input, outputs = self.output_text,
+                             title = "Audio Transcription App",
+                             description = "Upload the audio file")
 
     def start_app(self):
-        pass
+        self.iface.launch(server_name="127.0.0.1", server_port=7860)
--- Generative-AI-powered-weeting-assistant/main.py
+++ Generative-AI-powered-weeting-assistant/main.py
@@ -1,11 +1,6 @@
 """This is the main module  and will be used to run the program"""
+from interface import Interface
 
-
-from audio_downloader import AudioDownloader
-from speech_to_text import SpeechToText
-audio = AudioDownloader()
 url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-GPXX04C6EN/Testing%20speech%20to%20text.mp3"
-audio.download_file(url)
-audio_name = audio.get_audio_name()
-transcriber = SpeechToText()
-transcriber.transcribe(audio_name)
+app = Interface(url)
+app.start_app()
